7e279dea164fbe87ea2cd37dc774beccf57f6a5f
Victor Winberg <victor.m.winberg@gmail.com>
2019-01-28 15:15:35 +0100

    Add API urls route

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index_view(request):
+    return HttpResponse("Welcome to Index View.")
+
+def index(request):
+    return HttpResponse("Welcome to API.")
