7e8ffc6817cfc1f884f9cd7214d98f74f7e4b32c
Victor Winberg <victor.m.winberg@gmail.com>
2019-01-31 10:56:15 +0100

    Add CREATE in to_cypher

--- backend/db/to_cypher.py
+++ backend/db/to_cypher.py
@@ -1,5 +1,17 @@
+# Cypher-shell script command
+# > py .\to_cypher.py | cypher-shell -u neo4j -p password --format verbose
+
 import json
 import sys
+import re
+
+label = {
+    "companies": "Company",
+    "persons": "Person",
+    "offices": "Office",
+    "coworkers": "Coworker",
+    "deals": "Deal"
+}
 
 if __name__ == "__main__":
     filename = "output.json"
@@ -9,5 +21,15 @@ if __name__ == "__main__":
     f = open(filename)
     data = json.loads(f.read())
     f.close()
-    
-    print(data)
+
+    print('MATCH ()-[r]-() DELETE r;')
+    print('MATCH (n) DELETE n;')
+
+    queries = []
+
+    for key in data:
+        for node in data[key]:
+            props = re.sub(r'(?<!: )"(\S*?)"', '\\1', json.dumps(node))
+            queries.append('(:{} {})'.format(label[key], props))
+
+    print('CREATE {};'.format(','.join(queries)))
