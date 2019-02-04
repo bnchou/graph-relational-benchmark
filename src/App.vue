be99e5ac71ce1c984d3d7fe93e3ff5360350b7d4
Victor Winberg <victor.m.winberg@gmail.com>
2019-02-04 12:07:35 +0100

    Add Three v-charts examples

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>
+          <img alt="Vue logo" src="./assets/logo.png">
+          <HelloWorld title="Benchmark: Graph vs Relational DB"/>
+          <el-row :gutter="20">
+            <el-col :span="8">
+              <h3>Graph example</h3>
+              <LineExample/>
+            </el-col>
+            <el-col :span="8">
+              <h3>Gauge example</h3>
+              <GaugeExample/>
+            </el-col>
+            <el-col :span="8">
+              <h3>Pie example</h3>
+              <PieExample/>
+            </el-col>
+          </el-row>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import LineExample from "./components/LineExample.vue";
+import GaugeExample from "./components/GaugeExample.vue";
+import PieExample from "./components/PieExample.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    LineExample,
+    GaugeExample,
+    PieExample
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
