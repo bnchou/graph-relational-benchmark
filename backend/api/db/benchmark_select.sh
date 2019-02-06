6251fe97cc5f92a7b94b67a6c33b7dbcc6eba663
Victor Winberg <victor.m.winberg@gmail.com>
2019-02-06 11:59:02 +0100

    Variable namechange

--- backend/api/db/benchmark_select.sh
+++ backend/api/db/benchmark_select.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-START1=$(date +%s%3N)
+T1=$(date +%s%3N)
 
 cat "out/cypher/match_queries.cypher" | cypher-shell -u neo4j -p password --format verbose
 
-END1=$(date +%s%3N)
+T2=$(date +%s%3N)
 
-sqlcmd -d LimeDB -i out//sql/select_queries.sql
+sqlcmd -d LimeDB -i out/sql/select_queries.sql
 
-END2=$(date +%s%3N)
+T3=$(date +%s%3N)
 
-echo Time GraphDB: $(($END1 - $START1)) ms
+echo Time GraphDB: $(($T2 - $T1)) ms
 
-echo Time RelationalDB: $(($END2 - $END1)) ms
+echo Time RelationalDB: $(($T3 - $T2)) ms
