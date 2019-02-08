5e8d3588ddc78b36c0a238ba988bbb5870c929cc
Victor Winberg <victor.m.winberg@gmail.com>
2019-02-08 11:14:06 +0100

    Refactor read_transaction

--- backend/api/db/benchmark_cypher.py
+++ backend/api/db/benchmark_cypher.py
@@ -16,12 +16,12 @@ def random_entry(data, table_name, column):
     return random_row[column]
 
 
-def run_query(session, query, inputs=[]):
+def run_query(transaction, query, inputs=[]):
     def execute(tx):
         result = tx.run(query.format(*inputs)).consume()
         print("{} ms, {} ms".format(result.t_first, result.t_last))
 
-    session.read_transaction(execute)
+    transaction(execute)
 
 
 if __name__ == "__main__":
@@ -31,26 +31,26 @@ if __name__ == "__main__":
     data = load_data()
 
     with driver.session() as session:
-        run_query(session, '''
+        run_query(session.read_transaction, '''
             MATCH (c: Company)
             RETURN c.name, c.id;
         ''')
 
-        run_query(session, '''
+        run_query(session.read_transaction, '''
             MATCH (p:Person)-[:RESPONSIBLE_FOR]->(d: Deal)
             MATCH (p)-[WORKS_AT]->(c: Company)
             WHERE d.probability > {}
             RETURN p.name, p.position, p.email, p.phone, d.name, c.name;
         ''', [random_entry(data, 'deals', 'probability')])
 
-        run_query(session, '''
+        run_query(session.read_transaction, '''
             MATCH (deal:Deal)-[:PART_OF]->(history:History)<-[:ATTACHED_TO]-(document:Document)
             MATCH (coworker:Coworker)-[:ATTENDED]->(history)<-[:ATTENDED]-(person:Person)
             WHERE history.id = {}
             RETURN history.id, history.type, history.date, coworker.name, person.name, document.description;
         ''', [random_entry(data, 'histories', 'id')])
 
-        run_query(session, '''
+        run_query(session.read_transaction, '''
             MATCH (p: Person)-[:WORKS_AT]->(c:Company)
             WHERE c.id = {}
             RETURN p.name, c.name;
