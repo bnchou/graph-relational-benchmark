2d1447d2bf9e2f57a265582a835bbbbfba44a7cc
Jan Zubac <jan.zubac@gmail.com>
2019-02-08 13:25:05 +0100

    Add sql query with parameter in python

--- backend/api/db/benchmark_sql.py
+++ backend/api/db/benchmark_sql.py
@@ -1,11 +1,15 @@
 import pyodbc
 
-# Specifying the ODBC driver, server name, database, etc. directly
 cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=BS28M32;DATABASE=LimeDB;')
-
-# Create a cursor from the connection
 cursor = cnxn.cursor()
 
 cursor.execute("SELECT * FROM companies;")
+
+cursor.execute('''
+    SELECT p.name, p.position, p.email, p.phone, deals.name, companies.name
+    FROM persons AS p
+    LEFT JOIN deals ON p.id = deals.person_id
+    LEFT JOIN companies ON p.company_id = companies.id
+    WHERE deals.probability > ?''', 0.99)
 
 print(cursor.fetchall())
