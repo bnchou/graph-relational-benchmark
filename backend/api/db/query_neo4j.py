4a7b1ff3106f816ec6dd3aabdab52a44547d5c8c
Victor Winberg <victor.m.winberg@gmail.com>
2019-02-08 10:42:56 +0100

    Modify query script with t_first and t_last

--- backend/api/db/query_neo4j.py
+++ backend/api/db/query_neo4j.py
@@ -14,24 +14,19 @@ def get_persons_with_deal_prob(tx, prob):
         RETURN p.name, p.position, p.email, p.phone, d.name, c.name;
         '''.format(prob)
 
-    for record in tx.run(query):
-        p_name = record["p.name"].encode('UTF-8')
-        p_pos = record["p.position"].encode('UTF-8')
-        p_mail = record["p.email"].encode('UTF-8')
-        p_phone = record["p.phone"].encode('UTF-8')
-        d_name = record["d.name"].encode('UTF-8')
-        c_name = record["c.name"].encode('UTF-8')
+    result = tx.run(query).consume()
+    print("{} ms, {} ms".format(result.t_first, result.t_last))
 
-        print("{} | {} | {} | {} | {} | {}".format(p_name, p_pos, p_mail, p_phone, d_name, c_name))
 
 def get_companies(tx):
     query = '''
         MATCH (c: Company)
         RETURN c.name, c.id;
         '''
-        
-    for record in tx.run(query):
-        print("{} | {}".format(record["c.name"].encode('UTF-8'), record["c.id"]))
+
+    result = tx.run(query).consume()
+    print("{} ms, {} ms".format(result.t_first, result.t_last))
+
 
 def get_history(tx, id):
     query = '''
@@ -41,16 +36,9 @@ def get_history(tx, id):
         RETURN history.id, history.type, history.date, coworker.name, person.name, document.description;
         '''.format(id)
 
-    for record in tx.run(query):
-        h_id = record["history.id"]
-        h_type = record["history.type"]
-        h_date = record["history.date"]
-        c_name = record["coworker.name"]
-        p_name = record["person.name"]
-        d_desc = record["document.description"]
+    result = tx.run(query).consume()
+    print("{} ms, {} ms".format(result.t_first, result.t_last))
 
-        print("{} | {} | {} | {} | {} | {}".format(h_id, h_type, h_date, c_name, p_name, d_desc))
-        
 
 def get_company_persons(tx, id):
     query = '''
@@ -58,15 +46,14 @@ def get_company_persons(tx, id):
         WHERE c.id = {}
         RETURN p.name, c.name;'''.format(id)
 
-    for record in tx.run(query):
-        print("{} | {}".format(record["p.name"].encode('UTF-8'), record["c.name"].encode('UTF-8')))
+    result = tx.run(query).consume()
+    print("{} ms, {} ms".format(result.t_first, result.t_last))
+
 
 with driver.session() as session:
-    # t = time()
-    # session.read_transaction(get_companies)
-    # print((time() - t) * 1000)
-    #session.read_transaction(get_persons_with_deal_prob, q.rand_entry('deals', 'probability'))
-    t = time()
+    session.read_transaction(get_companies)
+    session.read_transaction(get_persons_with_deal_prob,
+                             q.rand_entry('deals', 'probability'))
     session.read_transaction(get_history, q.rand_entry('histories', 'id'))
-    print((time() - t) * 1000)
-    #session.read_transaction(get_company_persons, q.rand_entry('companies', 'id'))
+    session.read_transaction(
+        get_company_persons, q.rand_entry('companies', 'id'))
