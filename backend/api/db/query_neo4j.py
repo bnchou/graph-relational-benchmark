517c79a80860a9380b56a609e78fea077e48afcd
Victor Winberg <victor.m.winberg@gmail.com>
2019-02-08 11:12:04 +0100

    Refactor query_neo4j

--- backend/api/db/query_neo4j.py
+++ backend/api/db/query_neo4j.py
@@ -1,59 +1,57 @@
 from neo4j import GraphDatabase
-import query_randomizer as q
-from time import time
-
-uri = "bolt://localhost:7687"
-driver = GraphDatabase.driver(uri, auth=("neo4j", "password"))
+import json
+import random
 
 
-def get_persons_with_deal_prob(tx, prob):
-    query = '''
-        MATCH (p:Person)-[:RESPONSIBLE_FOR]->(d: Deal)
-        MATCH (p)-[WORKS_AT]->(c: Company)
-        WHERE d.probability > {}
-        RETURN p.name, p.position, p.email, p.phone, d.name, c.name;
-        '''.format(prob)
-
-    result = tx.run(query).consume()
-    print("{} ms, {} ms".format(result.t_first, result.t_last))
+def load_data(filepath="out/output.json"):
+    f = open(filepath, 'r')
+    data = json.loads(f.read())
+    f.close()
+    return data
 
 
-def get_companies(tx):
-    query = '''
-        MATCH (c: Company)
-        RETURN c.name, c.id;
-        '''
-
-    result = tx.run(query).consume()
-    print("{} ms, {} ms".format(result.t_first, result.t_last))
+def random_entry(data, table_name, column):
+    table = data[table_name]
+    random_row = table[random.randint(0, len(table))]
+    return random_row[column]
 
 
-def get_history(tx, id):
-    query = '''
-        MATCH (deal:Deal)-[:PART_OF]->(history:History)<-[:ATTACHED_TO]-(document:Document)
-        MATCH (coworker:Coworker)-[:ATTENDED]->(history)<-[:ATTENDED]-(person:Person)
-        WHERE history.id = {}
-        RETURN history.id, history.type, history.date, coworker.name, person.name, document.description;
-        '''.format(id)
+def run_query(session, query, inputs=[]):
+    def execute(tx):
+        result = tx.run(query.format(*inputs)).consume()
+        print("{} ms, {} ms".format(result.t_first, result.t_last))
 
-    result = tx.run(query).consume()
-    print("{} ms, {} ms".format(result.t_first, result.t_last))
+    session.read_transaction(execute)
 
 
-def get_company_persons(tx, id):
-    query = '''
-        MATCH (p: Person)-[:WORKS_AT]->(c:Company)
-        WHERE c.id = {}
-        RETURN p.name, c.name;'''.format(id)
+if __name__ == "__main__":
+    uri = "bolt://localhost:7687"
+    driver = GraphDatabase.driver(uri, auth=("neo4j", "password"))
 
-    result = tx.run(query).consume()
-    print("{} ms, {} ms".format(result.t_first, result.t_last))
+    data = load_data()
 
+    with driver.session() as session:
+        run_query(session, '''
+            MATCH (c: Company)
+            RETURN c.name, c.id;
+        ''')
 
-with driver.session() as session:
-    session.read_transaction(get_companies)
-    session.read_transaction(get_persons_with_deal_prob,
-                             q.rand_entry('deals', 'probability'))
-    session.read_transaction(get_history, q.rand_entry('histories', 'id'))
-    session.read_transaction(
-        get_company_persons, q.rand_entry('companies', 'id'))
+        run_query(session, '''
+            MATCH (p:Person)-[:RESPONSIBLE_FOR]->(d: Deal)
+            MATCH (p)-[WORKS_AT]->(c: Company)
+            WHERE d.probability > {}
+            RETURN p.name, p.position, p.email, p.phone, d.name, c.name;
+        ''', [random_entry(data, 'deals', 'probability')])
+
+        run_query(session, '''
+            MATCH (deal:Deal)-[:PART_OF]->(history:History)<-[:ATTACHED_TO]-(document:Document)
+            MATCH (coworker:Coworker)-[:ATTENDED]->(history)<-[:ATTENDED]-(person:Person)
+            WHERE history.id = {}
+            RETURN history.id, history.type, history.date, coworker.name, person.name, document.description;
+        ''', [random_entry(data, 'histories', 'id')])
+
+        run_query(session, '''
+            MATCH (p: Person)-[:WORKS_AT]->(c:Company)
+            WHERE c.id = {}
+            RETURN p.name, c.name;
+        ''', [random_entry(data, 'companies', 'id')])
