bf1f5327ccb21c85f786497b0476ee3af0ce209c
Victor Winberg <victor.m.winberg@gmail.com>
2019-02-08 11:38:37 +0100

    Add statistics method

--- backend/api/db/benchmark_cypher.py
+++ backend/api/db/benchmark_cypher.py
@@ -1,6 +1,7 @@
 from neo4j import GraphDatabase
+from statistics import median
+from random import randint
 import json
-import random
 
 
 def load_data(filepath="out/output.json"):
@@ -12,16 +13,20 @@ def load_data(filepath="out/output.json"):
 
 def random_entry(data, table_name, column):
     table = data[table_name]
-    random_row = table[random.randint(0, len(table))]
+    random_row = table[randint(0, len(table))]
     return random_row[column]
 
 
 def run_query(transaction, query, inputs=[]):
     def execute(tx):
         result = tx.run(query.format(*inputs)).consume()
-        print("{} ms, {} ms".format(result.t_first, result.t_last))
+        return result.t_first
 
-    transaction(execute)
+    return transaction(execute)
+
+
+def get_stats(exec, amount=100):
+    print(median([exec() for i in range(amount)]))
 
 
 if __name__ == "__main__":
@@ -31,27 +36,27 @@ if __name__ == "__main__":
     data = load_data()
 
     with driver.session() as session:
-        run_query(session.read_transaction, '''
-            MATCH (c: Company)
-            RETURN c.name, c.id;
-        ''')
+        get_stats(lambda: run_query(session.read_transaction, '''
+             MATCH (c: Company)
+             RETURN c.name, c.id;
+         '''))
 
-        run_query(session.read_transaction, '''
+        get_stats(lambda: run_query(session.read_transaction, '''
             MATCH (p:Person)-[:RESPONSIBLE_FOR]->(d: Deal)
             MATCH (p)-[WORKS_AT]->(c: Company)
             WHERE d.probability > {}
             RETURN p.name, p.position, p.email, p.phone, d.name, c.name;
-        ''', [random_entry(data, 'deals', 'probability')])
+        ''', [random_entry(data, 'deals', 'probability')]))
 
-        run_query(session.read_transaction, '''
+        get_stats(lambda: run_query(session.read_transaction, '''
             MATCH (deal:Deal)-[:PART_OF]->(history:History)<-[:ATTACHED_TO]-(document:Document)
             MATCH (coworker:Coworker)-[:ATTENDED]->(history)<-[:ATTENDED]-(person:Person)
             WHERE history.id = {}
             RETURN history.id, history.type, history.date, coworker.name, person.name, document.description;
-        ''', [random_entry(data, 'histories', 'id')])
+        ''', [random_entry(data, 'histories', 'id')]))
 
-        run_query(session.read_transaction, '''
+        get_stats(lambda: run_query(session.read_transaction, '''
             MATCH (p: Person)-[:WORKS_AT]->(c:Company)
             WHERE c.id = {}
             RETURN p.name, c.name;
-        ''', [random_entry(data, 'companies', 'id')])
+        ''', [random_entry(data, 'companies', 'id')]))
