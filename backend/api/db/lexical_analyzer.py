f02cda73e19eb03d7a55379ae21f9fbc313c0682
Victor Winberg <victor.m.winberg@gmail.com>
2019-02-11 12:03:57 +0100

    Add lexical analyzer

---
+++ backend/api/db/lexical_analyzer.py
@@ -0,0 +1,54 @@
+import re
+import json
+import sys
+
+if __name__ == "__main__":
+    query = "SELECT * FROM customers;"
+    f2 = "output.json"
+    if(len(sys.argv) > 2):
+        f1 = sys.argv[1]
+        f2 = sys.argv[2]
+
+        f = open(f1, 'r')
+        query = f.read()
+        f.close()
+    elif(len(sys.argv) > 1):
+        f2 = sys.argv[1]
+
+    parser = re.compile(r"""
+        ([0-9]+\.?[0-9]*|'.*')                  # 1. literals
+        |([{}()[\],:;-]|<-|->)                  # 2. seperators
+        |([A-Z]?[a-z]+\.?[a-z_]*|(?<=:)[A-Z_]+) # 3. identifiers
+        |(=|>=|>|<=|<|!=|\*)                    # 4. operators
+        |([A-Z]+)                               # 5. keywords
+        |(\s+)                                  # 6. whitespace
+        """, re.VERBOSE)
+
+    groups = [
+        "literals",
+        "seperators",
+        "identifiers",
+        "operators",
+        "keywords",
+        "whitespace"
+    ]
+
+    result = {group: {} for group in groups}
+
+    # bind a scanner to the target string
+    scan = parser.scanner(query)
+
+    # print all tokens
+    while 1:
+        m = scan.match()
+        if not m:
+            break
+        group = result[groups[m.lastindex - 1]]
+        token = m.group(m.lastindex)
+        group[token] = group.get(token, 0) + 1
+
+    # remove whitespace group from result dict
+    del result["whitespace"]
+
+    with open(f2, 'w') as f_out:
+        json.dump(result, f_out)
