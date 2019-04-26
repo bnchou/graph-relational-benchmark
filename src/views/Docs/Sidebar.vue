a77e9304054f20d2337714e71298099bdf594c3d
Jan Zubac <jan.zubac@gmail.com>
2019-04-26 11:03:41 +0200

    Cleanup, remove unused files

--- src/views/Docs/Sidebar.vue
+++ src/views/Docs/Sidebar.vue
@@ -6,12 +6,7 @@
         <span>Guide</span>
       </template>
       <el-menu-item index="/docs/intro">Introduction</el-menu-item>
-      <el-menu-item index="/docs/quick-start">Quick Start</el-menu-item>
     </el-submenu>
-    <el-menu-item index="/docs/components">
-      <i class="el-icon-menu"></i>
-      <span>Components</span>
-    </el-menu-item>
   </el-menu>
 </template>
 
